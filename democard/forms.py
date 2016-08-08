e4885aa6a1c77ffe76e9e5bf937fa18214d469ff
Pulumati Priyank <pulumati.priyank@gmail.com>
2016-08-09 01:06:42 +0530

    Changed Form Labels

--- democard/forms.py
+++ democard/forms.py
@@ -7,11 +7,11 @@ class IDForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(IDForm, self).__init__(*args, **kwargs)
         sex_choices = (
-            ('M', 'Male'),
-            ('F', 'Female'),
+            ('Male', 'Male'),
+            ('Female', 'Female'),
         )
         self.fields['first_name'] = forms.CharField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Fist Name"))
         self.fields['last_name'] = forms.CharField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Last Name"))
-        self.fields['id'] = forms.CharField(widget=forms.TextInput(attrs=dict(max_length=200)), label=_("Profile Image"))
+        self.fields['id'] = forms.CharField(widget=forms.TextInput(attrs=dict(max_length=200)), label=_("ID"))
         self.fields['age'] = forms.CharField(max_length=3, label=_("Age"))
         self.fields['sex'] = forms.ChoiceField(choices = sex_choices, label=_("Sex"))
